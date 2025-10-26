7ccd95a11e1162c740554b3ba49b000e076f1c35
Gram <erebea@live.com.ar>
2025-10-26 11:41:42 -0300

    se limpio el main, se agrego a clase de control de flujo

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,9 @@
-import enums.TipoGrupoMuscular;
-import gestores.GenericGestor;
-import models.database.ControlData;
-import models.rutinas.Ejercicio;
-import models.rutinas.Rutina;
-import models.users.Staff;
-import models.users.User;
-import models.utils.Utilidades;
-import org.json.JSONObject;
-
-import java.util.List;
+import models.App;
 
 public class Main {
     public static void main(String[] args) {
-        GenericGestor<User> usuarios = new GenericGestor<>();
-//        usuarios.actualizarGestor("usuarios");
-
-        GenericGestor<Rutina> rutinas = new GenericGestor<>();
-        rutinas.actualizarGestor("rutinas");
-
-        GenericGestor<Ejercicio> ejercicios = new GenericGestor<>();
-        ejercicios.actualizarGestor("ejercicios");
-
-        System.out.println(ejercicios.getClass().getTypeName());
+        App GestorGym = new App();
+        GestorGym.inicializar();
     }
 
 }
--- src/models/App.java
+++ src/models/App.java
@@ -1,7 +1,21 @@
 package models;
 
-public class App {
+import gestores.GenericGestor;
+import models.rutinas.Ejercicio;
+import models.rutinas.Rutina;
+import models.users.User;
 
+public class App {
+    public void inicializar(){
+        GenericGestor<User> usuarios = new GenericGestor<>();
+//        usuarios.actualizarGestor("usuarios");
+
+        GenericGestor<Rutina> rutinas = new GenericGestor<>();
+        rutinas.actualizarGestor("rutinas");
+
+        GenericGestor<Ejercicio> ejercicios = new GenericGestor<>();
+        ejercicios.actualizarGestor("ejercicios");
+    }
 
 
 }
